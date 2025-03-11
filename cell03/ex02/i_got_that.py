117b6222b689280184d9ea0c973bdeef81d4a95c
TcatTMC <gasithitep@gmail.com>
2025-03-11 11:18:47 +0700

    Add files via upload

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,8 @@
+say = input("What you gotta say? : ")
+ 
+while True :
+    say = input("I got that!,Anything else? : ")
+
+    if say.lower() == 'stop' :
+        print("program stop")
+        break
